963ae04fb17d1ecaf247f4751a950efb40c95d7f
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-03-15 23:19:37 +0900

    script.sh ; procの数が5個に増えた。メモも改訂。

--- bin/script.sh
+++ bin/script.sh
@@ -12,13 +12,15 @@ sleep $(( $RANDOM % 3 )) ; # ランダムな秒数待ってみる。3秒以内�
 proc1="dl2gitrepo $fetched https://www.mhlw.go.jp/content/life_welfare_small_fund.csv 緊急小口資金等の特例貸付"
 proc2="copyDTfile -U $fetched/life_welfare_small_fund.csv $renamed"
 proc3="cd $renamed ; csv2onetsv -m -s1 -t0 -E 'LWSF22{0126,0202,0307}T????.csv' LWSF2?????T????.csv >| $binded/since220204.csv"
+proc4="git add $binded/since220204.csv ; git commit -m 'since220204.csv created.' "; 
+proc5="dufolder -G1 `git rev-parse --show-superproject-working-tree --show-toplevel` ; git push "  ;
+eval $proc1 && { eval $proc2 ; eval $proc3 ; eval $proc4 ; eval $proc5 } 
 
-eval $proc1 && { eval $proc2 ; eval $proc3 } 
-dufolder -G1 `git rev-parse --show-superproject-working-tree --show-toplevel`
 
 
 # 開発上や使用する上でのメモ
 #
-#  新しいサーバーで、このレポジトリに含まれているプログラムを実行することになる。
-#  この場合、このスクリプトだけ普段はcrontabから実行することになる。
-#  ただし、新しいサーバーで始めるにしても、最初にUNIX系の簡単なコマンドをいろいろ使った初期操作が必要であろう。
+#  
+#  このスクリプトをcrontabから実行できるように書くようにつとめた。
+#  
+#  2022-03-15 下野寿之(統計数理研究所 特任研究員)
