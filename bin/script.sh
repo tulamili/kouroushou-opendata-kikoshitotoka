75e58740474bcaf121b83a76e3b2d162cabc0c53
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2022-03-15 23:33:32 +0900

    script.sh crontabで動かせるように書き足し。

--- bin/script.sh
+++ bin/script.sh
@@ -8,13 +8,13 @@ export PATH=$selfdir:$PATH ; # パスの設定
 export PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}" ; # cpanmを利用していろいろなモジュールをインストールしているため
 sleep $(( $RANDOM % 3 )) ; # ランダムな秒数待ってみる。3秒以内で。
 
-
+cd $selfdir 
 proc1="dl2gitrepo $fetched https://www.mhlw.go.jp/content/life_welfare_small_fund.csv 緊急小口資金等の特例貸付"
 proc2="copyDTfile -U $fetched/life_welfare_small_fund.csv $renamed"
 proc3="cd $renamed ; csv2onetsv -m -s1 -t0 -E 'LWSF22{0126,0202,0307}T????.csv' LWSF2?????T????.csv >| $binded/since220204.csv"
 proc4="git add $binded/since220204.csv ; git commit -m 'since220204.csv created.' "; 
 proc5="dufolder -G1 `git rev-parse --show-superproject-working-tree --show-toplevel` ; git push "  ;
-eval $proc1 && { eval $proc2 ; eval $proc3 ; eval $proc4 ; eval $proc5 } 
+eval $proc1 && { eval $proc2 ; eval $proc3 ; eval $proc4 > /dev/null 2>&1 ; eval $proc5 > /dev/null 2>&1 } 
 
 
 
